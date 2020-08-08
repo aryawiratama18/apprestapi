36aae1206cc6cc0b1d7058d77c52f8f47f5d1f92
aryawiratama18 <NAwiratama@gmail.com>
2020-08-08 07:27:46 +0700

    Showing all mahasiswa data

--- controller.js
+++ controller.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
 
-var response = require('./res');
-var connection = require('./koneksi');
+var response = require("./res");
+var connection = require("./koneksi");
+const conn = require("./koneksi");
 
 exports.index = function (req, res) {
-    response.ok("Aplikasi REST API berjalan")
+  response.ok("Aplikasi REST API berjalan", res);
+};
+
+// menampilkan semua data mahasiswa
+exports.tampilsemuadatamahasiswa = function (req, res) {
+  connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
 };
