6452b4bf1cc826e732e20d3d7bcb20fd76877e31
aryawiratama18 <NAwiratama@gmail.com>
2020-08-08 07:39:34 +0700

    Succes showing data base on id

--- controller.js
+++ controller.js
@@ -5,16 +5,29 @@ var connection = require("./koneksi");
 const conn = require("./koneksi");
 
 exports.index = function (req, res) {
-  response.ok("Aplikasi REST API berjalan", res);
+    response.ok("Aplikasi REST API berjalan", res);
 };
 
 // menampilkan semua data mahasiswa
 exports.tampilsemuadatamahasiswa = function (req, res) {
-  connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
-    if (error) {
-      connection.log(error);
-    } else {
-      response.ok(rows, res);
-    }
-  });
+    connection.query("SELECT * FROM mahasiswa", function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
 };
+
+// menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function (req, res) {
+    let id = req.params.id;
+    connection.query("SELECT * FROM mahasiswa WHERE id = ?", id,
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
+}
